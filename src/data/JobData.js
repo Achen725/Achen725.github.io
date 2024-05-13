1a83cf518255afb6a796c62de9eaabaf56b83ae4
Andy Chen <ac725.cfl@gmail.com>
2024-05-13 06:43:02 -0700

    Redact certain info

--- src/data/JobData.js
+++ src/data/JobData.js
@@ -11,16 +11,7 @@ export const JobList = [
         title: "Amazon SDE 1",
         description: "Software Developer for Amazon Astro, a household robot.",
         projects: [
-            {
-                project_name: "Stealth",
-                description: "Stealth",
-                tech_stack: "Java, AWS (AppConfig, SageMaker, Bedrock, ECS)",
-            },
-            {
-                project_name: "Stealth",
-                description: "Led development and technical ownership for an internal tool to manage assets on device.",
-                tech_stack: "Javascript, AWS (DynamoDb and S3)",
-            },
+    
         ],
       },      
 ];
